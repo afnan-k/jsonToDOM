ffaf117a33065531379c1644d875fd14e9d4ae90
Afnan Khan <afnan_khan@outlook.com>
2016-11-26 18:56:50 +0500

    Update jsonToDOM.js

--- jsonToDOM.js
+++ jsonToDOM.js
@@ -1,1 +1,44 @@
-function jsonToDOM(args) {  return new Promise((resolve) => {    const createElement = (tagName, attributes) => {      let key;      const element = document.createElement(tagName);      for (key in attributes) {        if (typeof attributes[key] === 'function') {          element.addEventListener(key.replace(/^on/, ''), attributes[key], false);        } else {          element.setAttribute(key, attributes[key]);        }      }      return element;    };    const generateDOM = (array) => {      if (!Array.isArray(array)) {        return document.createTextNode(array);      }      if (typeof array[0] === 'string' && array[1] && array[1].constructor === Object) {        const element = createElement(array[0], array[1]);        if (array[2] !== undefined) {          const node = generateDOM(array[2]);          element.appendChild(node);        }        return element;      }      const dom = document.createDocumentFragment();      for (let i = 0, l = array.length; i < l; i++) {        if (array[i] !== undefined) {          const node = generateDOM(array[i]);          dom.appendChild(node);        }      }      return dom;    };    resolve(generateDOM(args));  }, e => console.error(e));}
+function jsonToDOM(args) {
+  return new Promise((resolve) => {
+    const createElement = (tagName, attributes) => {
+      let key;
+      const element = document.createElement(tagName);
+
+      for (key in attributes) {
+        if (typeof attributes[key] === 'function') {
+          element.addEventListener(key.replace(/^on/, ''), attributes[key], false);
+        } else {
+          element.setAttribute(key, attributes[key]);
+        }
+      }
+
+      return element;
+    };
+
+    const generateDOM = (array) => {
+      if (!Array.isArray(array)) {
+        return document.createTextNode(array);
+      }
+
+      if (typeof array[0] === 'string' && array[1] && array[1].constructor === Object) {
+        const element = createElement(array[0], array[1]);
+        if (array[2] !== undefined) {
+          const node = generateDOM(array[2]);
+          element.appendChild(node);
+        }
+        return element;
+      }
+
+      const dom = document.createDocumentFragment();
+      for (let i = 0, l = array.length; i < l; i++) {
+        if (array[i] !== undefined) {
+          const node = generateDOM(array[i]);
+          dom.appendChild(node);
+        }
+      }
+      return dom;
+    };
+
+    resolve(generateDOM(args));
+  }, e => console.error(e));
+}
